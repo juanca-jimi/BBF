eac90801e173a61eae1c29bb1097b83ecd23e94d
jordan surgick <jordansurgick27@gmail.com>
2022-07-20 15:47:30 -0400

    added 2 packages and main class, controller, gitignore, and textparser

---
+++ src/com/bbf/client/Main.java
@@ -0,0 +1,16 @@
+package com.bbf.client;
+
+import com.bbf.controller.BbfController;
+
+import java.io.FileNotFoundException;
+
+public class Main {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        //BbfController controller = new BbfController();
+        //controller.execute();
+        /*
+        Main file to run our program
+         */
+    }
+}
