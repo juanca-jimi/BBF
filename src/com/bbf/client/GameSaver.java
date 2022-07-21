e4161dd41e8bae1f7738dd595fc51077f3ac2323
JuanCarlos Jimenez <jcj237@outlook.com>
2022-07-21 12:57:41 -0400

    Outlines which room the player is in, which items are the room, and the possibles routes in the room AB#11. GameSaver class receives todos to outline future work needed in the class.

--- src/com/bbf/client/GameSaver.java
+++ src/com/bbf/client/GameSaver.java
@@ -15,16 +15,20 @@ public class GameSaver implements Helpable
 
     public static boolean saveGameQuestion()
     {
+        // TODO: 7/21/22 give prompt and then possibly returns true if the user wants to save 
         return false;
     }
 
     public static boolean hasSavedGames()
     {
+        // TODO: 7/21/22 returns true if there a list of games 
         return false;
     }
 
     public static boolean playSavedGames()
     {
+        // TODO: 7/21/22 returns true if the user wants to play saved games
+        // must give prompt
         return false;
     }
 
