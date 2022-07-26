cf89d74ff3b93bf58dbb999622cb2889bab32475
JuanCarlos Jimenez <jcj237@outlook.com>
2022-07-26 13:13:14 -0700

    Adding functionality to the TextParser class. Exception classes are stubbed out and being executed for corresponding cases AB#14. Moving room methods now functional AB#12, what's left is integratin JSON resources. Textparser now implements helpable interface to list command. Functionality to request commands are needed in the constructor. AB#13 Methods are stubbed out for get and look commands, implementations are noted with todos AB#21 AB#10

--- src/main/java/com/bbf/client/TextParser.java
+++ src/main/java/com/bbf/client/TextParser.java
@@ -1,53 +1,166 @@
-package com.bbf.client;
+package com.game.controller;
+
+import com.game.client.StartGame;
+import com.game.error.InvalidCommandException;
+import com.game.error.InvalidGetException;
+import com.game.error.InvalidLocationException;
+import com.game.error.InvalidLookException;
 
 import java.util.Scanner;
 //import java.io.StreamTokenizer;
 
-public class TextParser {
-    public String item = null;
-    public String location = null;
-    //private Player player = null;
-    private final Scanner scanner = new Scanner(System.in);
+public class TextParser implements Helpable
+{
+    //CLASS FIELDS--------------------------------------------------------------
+    private final String command;
 
-    public TextParser(String item, String location) {
-        this.item = item;
-        this.location = location;
+    //CONSTRUCTOR---------------------------------------------------------------
+    public TextParser()
+    {
+        System.out.println("Enter direction here: ");
+        Scanner scanner = new Scanner(System.in);
+        command = scanner.nextLine().trim();
     }
 
-    private String textParser() {
-        basicGameInfo();
+    //METHODS-------------------------------------------------------------------
+    public void execute(StartGame.Game myGame, Location[] locations, Item[] items)
+    {
+        //THIS METHOD
+        //CHECKS FOR ALL COMMANDS
+        if (isGoCommand())
+            if(isValidLocation(locations))
+                move(locations, myGame);
+
+            else if (isGetCommand())
+                if(isValidItem(items))
+                    storeItem(myGame, items);
+
+                else if (isLookCommand())
+                    if(isValidItem(items))
+                        lookAtItem(myGame, items);
+                    else if(isValidLocation(locations))
+                        lookAtLocation(myGame, locations);
+                    else
+                        try
+                        {
+                            throw new InvalidLookException(command);
+                        }
+                        finally
+                        {
+                            return;
+                        }
+                else
+                    try
+                    {
+                        throw new InvalidCommandException(command);
+                    }
+                    //To continue execution of game
+                    finally
+                    {
+                        return;
+                    }
+    }
+
+    private void lookAtLocation(StartGame.Game myGame, Location[] locations)
+    {
+        // TODO: 7/26/22
+    }
+
+    private void lookAtItem(StartGame.Game myGame, Item[] items)
+    {
+        //TODO:
+    }
+
+    private boolean isLookCommand()
+    {
+        // TODO: 7/26/22
+        return false;
+    }
+
+    private void storeItem(StartGame.Game myGame, Item[] items)
+    {
+        myGame.storeItem(getItemFromCommand(command));
+    }
+
+    private String getItemFromCommand(String command)
+    {
         return null;
     }
 
-    private String basicGameInfo() {
-        String message1 = "Go [location]" + getLocation() + "Get [item]" + getItem();
-        String userInput = scanner.nextLine().toLowerCase().trim();
-        if(userInput == userInput) {
-            System.out.println("get more info placeholder");
-        } else {
-//            message1;
-//            userInput;
+    private boolean isValidItem(Item[] items)
+    {
+        //TODO check if item from given
+        if(false)
+            return true;
+        else
+            try
+            {
+                throw  new InvalidGetException();
+            }
+            finally
+            {
+                return false;
+            }
+    }
+
+    private boolean isGetCommand()
+    {
+        return false;
+    }
+
+    public void move(Location[] locations, StartGame.Game myGame)
+    {
+        if (isValidLocation(locations))
+            //TODO: get the location from the whole command
+            myGame.updateGameRoom(getLocationFromCommand(command));
+
+    }
+
+    private boolean isValidLocation(Location[] locations)
+    {
+        command.trim().toLowerCase();
+
+        for (var validPath : locations)
+            //using endswith so that if user types in "go to x" it'll still work
+            if (command.endsWith(validPath.toString()))
+                return true;
+
+        //else
+        System.out.println("Invalid Location!");
+        showValidLocations(locations);
+        try
+        {
+            throw new InvalidLocationException(getLocationFromCommand(command), locations);
+        } finally
+        {
+            return false;
         }
-        return null;
     }
 
-    public String getItem() {
-        return item;
+    private String getLocationFromCommand(String command)
+    {
+        String[] words = command.split(" ");
+
+        //Returns the last word in the command
+        return words[words.length-1];
     }
 
-    public void setItem(String item) {
-        this.item = item;
+    private void showValidLocations(Location[] locations)
+    {
+        System.out.println("The possible locations to take are...");
+        for (var location : locations)
+            System.out.println("\t" + location);
     }
 
-    public String getLocation() {
-        return location;
+    private boolean isGoCommand()
+    {
+        //TODO: CHECKS FOR Synonyms
+        if (command.trim().toLowerCase().matches("^go{1}")) //TODO: VERIFTY REGEX WITH SENIOR ENG
+            return true;
+        return false;
     }
 
-    public void setLocation(String location) {
-        this.location = location;
+    public void helpscript(){
+
     }
 }
-
-/*
-Textparser class
- */
